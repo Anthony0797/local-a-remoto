78e704a7d50bcfff42c5fff48e850d675834936e
Anthony <anthony@prueba.com>
2022-05-02 19:18:41 +0200

    commit usuario2

--- src/ejercicio1/Lista.java
+++ src/ejercicio1/Lista.java
@@ -1,10 +1,11 @@
 package ejercicio1;
 
 public interface Lista {
-	public void putInicio(Object o);
+	public void putInicio(int o);
 	public void putFinal(Object o);
 	public Object popPrincipio();
 	public Object popFinal();
 	public Object getAt(int posicion);
 	public int count();
+	//Modificando el metodo putInicio
 }
